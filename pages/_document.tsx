e313e7f112e7bc00368f18bcde006837768e8e50
Hervé Rivière <HerveRiviere@users.noreply.github.com>
2023-10-27 22:09:19 +0200

    add analytics

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react'
 import Document, { Head, Html, Main, NextScript } from 'next/document'
+import { Analytics } from '@vercel/analytics/react';
 
 import { IconContext } from '@react-icons/all-files'
 
@@ -64,6 +65,7 @@ export default class MyDocument extends Document {
             <Main />
 
             <NextScript />
+            <Analytics />
           </body>
         </Html>
       </IconContext.Provider>
